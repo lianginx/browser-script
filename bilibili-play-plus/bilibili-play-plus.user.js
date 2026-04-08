d467cec46b8354c36ae82ff3a50f94b3ec7d4409
Liang <liang@in-x.cc>
2026-04-08 22:23:12 +0800

    精简重构脚本，调整栏目位置

--- bilibili-play-plus/bilibili-play-plus.user.js
+++ bilibili-play-plus/bilibili-play-plus.user.js
@@ -1,86 +1,79 @@
 // ==UserScript==
-// @name        哔哩哔哩增强
+// @name        优化哔哩哔哩播放体验
 // @namespace   @lianginx/bilibili-play-plus
 // @match       https://www.bilibili.com/video/*
 // @icon        https://i0.hdslb.com/bfs/static/jinkela/long/images/favicon.ico
 // @grant       none
-// @version     0.2.2
+// @version     0.3.0
 // @author      lianginx
-// @description 优化 B 站视频页播放体验，自动宽屏、取消顶部栏固定，并在播放时自动滚动到播放器。
+// @description 优化 B 站视频页播放体验。
 // @downloadURL https://raw.githubusercontent.com/lianginx/browser-script/refs/heads/master/bilibili-play-plus/bilibili-play-plus.user.js
 // @updateURL   https://raw.githubusercontent.com/lianginx/browser-script/refs/heads/master/bilibili-play-plus/bilibili-play-plus.user.js
 // ==/UserScript==
 
 'use strict';
 
-(function main() {
+main();
+
+function main() {
   const tasks = [
-    { action: cancelHeaderFixed, status: false },
-    { action: scrollToPlay, status: false },
     { action: autoWide, status: false },
+    { action: adjustPosition, status: false },
   ];
-
   const observer = new MutationObserver(() => {
-    tasks.forEach(task => {
-      if (!task.status) {
-        task.status = task.action();
-      }
-    });
-    if (tasks.every(task => task.status)) {
-      observer.disconnect();
+    for (const task of tasks) {
+      if (task.status) return;
+      task.status = task.action();
     }
+    if (tasks.every(task => task.status))
+      observer.disconnect();
   });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-})();
-
-// 取消 header 固定
-function cancelHeaderFixed() {
-  const style = document.createElement('style');
-  style.textContent = `
-  .bili-header.fixed-header .bili-header__bar {
-    position: static !important;
-    box-shadow: none !important;
-    border-bottom: 1px solid #ebebeb;
-  }
-  .v-popover {
-    z-index: 99 !important;
-  }
-  `;
-  document.head.appendChild(style);
-  return true;
-}
+  observer.observe(document.body, { childList: true, subtree: true });
+};
 
 // 自动宽屏
 function autoWide() {
   const targetSelector = '.bpx-player-ctrl-btn.bpx-player-ctrl-wide';
   const btn = document.querySelector(targetSelector);
-  if (btn) {
-    btn.click();
-    return true;
-  }
-  return false;
+  if (!btn) return false;
+  btn.click();
+  return true;
 }
 
-// 播放视频时滚动到合适位置
-function scrollToPlay() {
-  const videoSelector = '#bilibili-player video';
-  const videoElement = document.querySelector(videoSelector);
-  if (videoElement) {
-    videoElement.addEventListener('play', scrollToVideo);
-    return true;
-  }
-  return false;
-}
+// 调整标题、UP 信息位置
+function adjustPosition() {
+  const title = document.querySelector('#viewbox_report')
+  const container = document.querySelector('.left-container.scroll-sticky')
+  const action = document.querySelector('#arc_toolbar_report')
+  const play = document.querySelector('#playerWrap')
+  const danmukuBox = document.querySelector('#danmukuBox')
 
-// 滚动视频元素到合适位置
-function scrollToVideo() {
-  const videoSelector = '#bilibili-player';
-  const videoElement = document.querySelector(videoSelector);
-  if (videoElement) {
-    setTimeout(() => videoElement.scrollIntoView({ block: 'center' }), 1000);
-  }
+  play.style.marginTop = '1.2rem'
+  danmukuBox.style.marginTop = 0
+
+  // 移动标题栏到视频下方
+  container.moveBefore(title, action)
+
+  // 宽屏模式下，调整 UP 信息位置
+  const upPanelContainer = document.querySelector('.up-panel-container')
+  const playHeightObserver = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      upPanelContainer.style.marginTop = `${entry.target.scrollHeight + 20}px`
+    }
+  });
+
+  // 宽屏模式下，调整右栏顶部外边距
+  const playWidthObserver = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      if (entry.target.scrollWidth > title.scrollWidth) {
+        playHeightObserver.observe(play)
+      } else {
+        playHeightObserver.unobserve(play)
+        upPanelContainer.style.marginTop = 0
+      }
+    }
+  });
+  playWidthObserver.observe(play)
+
+  return true
 }
