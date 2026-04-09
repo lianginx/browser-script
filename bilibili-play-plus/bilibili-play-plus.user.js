92a7e6a01a3db25d9825cdb64686e574aaa1ca21
Liang <liang@in-x.cc>
2026-04-09 13:12:02 +0800

    优化日志打印

--- bilibili-play-plus/bilibili-play-plus.user.js
+++ bilibili-play-plus/bilibili-play-plus.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.bilibili.com/video/*
 // @icon        https://i0.hdslb.com/bfs/static/jinkela/long/images/favicon.ico
 // @grant       none
-// @version     0.3.0
+// @version     0.3.1
 // @author      lianginx
 // @description 优化 B 站视频页播放体验。
 // @downloadURL https://raw.githubusercontent.com/lianginx/browser-script/refs/heads/master/bilibili-play-plus/bilibili-play-plus.user.js
@@ -33,47 +33,80 @@ function main() {
 
 // 自动宽屏
 function autoWide() {
-  const targetSelector = '.bpx-player-ctrl-btn.bpx-player-ctrl-wide';
-  const btn = document.querySelector(targetSelector);
-  if (!btn) return false;
-  btn.click();
-  return true;
+  try {
+    console.groupCollapsed('[优化哔哩哔哩播放体验] [自动宽屏]')
+    console.log('前置检测中…')
+
+    const wideBtn = document.querySelector('.bpx-player-ctrl-btn.bpx-player-ctrl-wide');
+    if (!wideBtn) throw new Error('宽屏按钮未找到');
+
+    wideBtn.click();
+    console.log('宽屏已打开');
+
+    return true;
+  } catch (err) {
+    console.log(err.message);
+    return false;
+  } finally {
+    console.groupEnd();
+  }
 }
 
-// 调整标题、UP 信息位置
+// 置顶播放器
 function adjustPosition() {
-  const title = document.querySelector('#viewbox_report')
-  const container = document.querySelector('.left-container.scroll-sticky')
-  const action = document.querySelector('#arc_toolbar_report')
-  const play = document.querySelector('#playerWrap')
-  const danmukuBox = document.querySelector('#danmukuBox')
+  try {
+    console.groupCollapsed('[优化哔哩哔哩播放体验] [置顶播放器]')
+    console.log('前置检测中…')
 
-  play.style.marginTop = '1.2rem'
-  danmukuBox.style.marginTop = 0
+    const title = document.querySelector('#viewbox_report')
+    const container = document.querySelector('.left-container.scroll-sticky')
+    const action = document.querySelector('#arc_toolbar_report')
+    const play = document.querySelector('#playerWrap')
+    const danmukuBox = document.querySelector('#danmukuBox')
 
-  // 移动标题栏到视频下方
-  container.moveBefore(title, action)
+    if (!title) throw new Error('标题栏未找到')
+    if (!container) throw new Error('容器栏未找到')
+    if (!action) throw new Error('操作栏未找到')
+    if (!play) throw new Error("播放器元素未找到");
+    if (!danmukuBox) throw new Error("弹幕栏未找到");
 
-  // 宽屏模式下，调整 UP 信息位置
-  const upPanelContainer = document.querySelector('.up-panel-container')
-  const playHeightObserver = new ResizeObserver((entries) => {
-    for (let entry of entries) {
-      upPanelContainer.style.marginTop = `${entry.target.scrollHeight + 20}px`
-    }
-  });
+    console.log('开始优化…');
 
-  // 宽屏模式下，调整右栏顶部外边距
-  const playWidthObserver = new ResizeObserver((entries) => {
-    for (let entry of entries) {
-      if (entry.target.scrollWidth > title.scrollWidth) {
-        playHeightObserver.observe(play)
-      } else {
-        playHeightObserver.unobserve(play)
-        upPanelContainer.style.marginTop = 0
+    // 移动标题栏到视频下方
+    container.moveBefore(title, action)
+
+    // 调整位置后优化样式
+    play.style.marginTop = '1.2rem'
+    danmukuBox.style.marginTop = 0
+
+    // 宽屏模式下，调整 UP 信息位置
+    const upPanelContainer = document.querySelector('.up-panel-container')
+    const playHeightObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        upPanelContainer.style.marginTop = `${entry.target.scrollHeight + 20}px`
       }
-    }
-  });
-  playWidthObserver.observe(play)
+    });
 
-  return true
+    // 宽屏模式下，调整右栏顶部外边距
+    const playWidthObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        if (entry.target.scrollWidth > title.scrollWidth) {
+          playHeightObserver.observe(play)
+        } else {
+          playHeightObserver.unobserve(play)
+          upPanelContainer.style.marginTop = 0
+        }
+      }
+    });
+    playWidthObserver.observe(play)
+
+    console.log('优化完成');
+
+    return true;
+  } catch (err) {
+    console.log(err.message);
+    return false;
+  } finally {
+    console.groupEnd();
+  }
 }
